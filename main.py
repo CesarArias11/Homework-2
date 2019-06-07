13de025d21b619330ca4d69977b9843e94d6bb42
CesarArias11 <a01411995@itesm.mx>
2019-06-06 23:04:07 -0500

    initial

---
+++ main.py
@@ -0,0 +1,31 @@
+
+def artist():
+    print("Paramore")
+    return
+
+
+def album():
+    print("Riot!")
+    return
+
+
+def duration():
+    print("3:11")
+    return
+
+
+def another_function(var):
+    if var == "Write":
+        print(True)
+    else:
+        print(False)
+
+
+artist()
+album()
+duration()
+another_function("vjgvjhvh")
+
+
+
+
